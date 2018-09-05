e6136d6937902b83647a1f4a310a75db076dbc56
DianaOlvera <DianaOlvera@users.noreply.github.com>
2018-09-04 22:10:55 -0500

    separando codigo en diferentes funciones y extrayendo url

--- src/app.js
+++ src/app.js
@@ -1,74 +1,42 @@
-// const fs = require('fs')
-// const readReadme = (callBack) => {
-//   fs.readFile('../README.md', 'utf8', (err, data) => {
-//     if (err) {
-//       console.log('Tienes un error')
-//     } else {
-//       callBack(data)
-//     }
-//   })
-// }
-// readReadme(callBack = (data) => {
-//   console.log(data)
-// })
 
-// /*  const data = fs.readFileSync('./Readme.md', 'utf8')
-// fs.watch('./', (eventType, fileName) => {
-//   console.log('Tipo de evento ' + eventType)
-//   if (fileName) {
-//     console.log('En el archivo ' + fileName)
-//   } else {
-//     console.log('No tienes cambio en los archivos.')
-//   }
-// }) */
-
-// const countLine = (err, data) => {
-//   if (err) {
-//     console.log('No tienes Readme.md')
-//   } else {
-//     let lines = data.split('\n').length
-//     console.log('El archivo tiene:' + lines + ' lineas')
-//   }
-// }
-// fs.readFile('../README.md', 'utf8', countLine)
-// module.exports = {
-//   countLine
-// }
 
 //let urlArray = []
-let patron = new RegExp('^http[s]?://\w[\.\w]+$', 'i')
+let exp = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
 let findUrl = ''
+let urlArray= []
 
-const fs = require('fs')
-const readReadme = (callBack) => {
+const fs = require('fs');
+
+const Readme = (callBack) => {
   fs.readFile('../Readme.md', 'utf8', (err, data) => {
     if (err) {
       console.log('Tienes un error')
     } else {
       callBack(data)     
     }
-    // console.log(urlArray)
   })
 }
-readReadme(callBack = (data) => {
-   
+Readme(callBack = (data) => {
+  console.log(data)
   for (let i = 0; i < data.length; i++) {
-    
       let total = data[i];
       findUrl += total;
-      console.log(findUrl)
+     // console.log(findUrl)
     }
-    const validate =()=>{
-      //console.log()
-        let find = findUrl.match(patron);
-        if (find){
-        let req = find;
-        }else{
-          console.log('error no entra al arreglo');
-        }
+    
+  
+
+  
+
+  const validate =()=>{
+    let find = findUrl.match(exp);// hace la busqueda
+    //console.log(find)
+    if (find){
+    urlArray.push(find);
+   console.log(urlArray)
+    }else{
+     // console.log('error, verifica');
     }
-    validate();
-  })
-
-
-
+}
+validate();
+});
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 const data = require('./app.js')
 
-const readFileSync = require('./app.js')
 
-const countLine = require('./app.js')
