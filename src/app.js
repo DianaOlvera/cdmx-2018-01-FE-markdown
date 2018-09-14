5b00ff6f2045de1e64ad06589fd34a7f9b181d90
DianaOlvera <DianaOlvera@users.noreply.github.com>
2018-09-14 13:59:10 -0500

    funcionando la promesa que obtiene la data

--- src/app.js
+++ src/app.js
@@ -1,94 +1,198 @@
-let exp = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
-let string = /\[([^\]]*)\]/g
-let findUrl = ''
-let findString = ''
-let urlArray= []
+//const exp = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+//const string = /\[([^\]]*)\]/g
+//let findUrl = ''
+let urlArraysuccess= []
+let urlArrayfail=[]
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-const Readme = (callBack) => {
-  fs.readFile('../Readme.md', 'utf8', (err, data) => {
-    if (err) {
-      console.log('Tienes un error')
-    } else {
-      callBack(data)     
-    }
+const Readme = (urlFile) => {
+  return new Promise(function(resolve,reject){
+    fs.readFile(urlFile, 'utf8', (err, data) => {
+      if (err) {
+        return reject (err)
+      }
+        resolve(data)     
+        console.log(data)    
+    })
   })
 }
-Readme(callBack = (data) => {
-  for (let i = 0; i < data.length; i++) {
-      let total = data[i];
-      findUrl += total;
-      
-    }
-  validate();
-  });
 
-  const validate =()=>{
-    let find = findUrl.match(exp);// hace la busqueda de url
-    //console.log(find)
-    let finds = findUrl.match(string);// hace busqueda de titulo
-    //console.log(finds)
-    
-    if(find){
-     for(let i=0; i< find.length; i++){
-       let request = find[i];
-       fetch(request).then((res)=> {
-         let status = res.status;
-         const extracttUrl = {
-          Name: finds[i],
-          Url: find[i],
-          Status: status
-         }
-         if(status == 200){
-          
-        console.log('Url exitosa: ' + ' ' + extracttUrl.Name +' '+ extracttUrl.Url + '  status: ' + extracttUrl.Status);
-         }else{
-          //console.log('fail' + extracttUrl);
-         }         
-       }).catch((err)=>{
-         console.log(err.message);
-       })
-     }
-   }
-//urlArray.push(find);
-//console.log(urlArray)
-}
-
-
-
-
-// const urlFinder = () => {
-//   findUrl = dataIterada.match(patron)
-//   findName = dataIterada.match(urlName)
-//   // console.log(findName)
-//   if (findUrl) {
-//     for (let i = 0; i < findUrl.length; i++) {
-//       let request = findUrl[i]
-//       fetch(request).then((response) => {
-//         // console.log(response)
-//         let status = response.status
-//         if (status == 200) {
-//           console.log('ok')
-//         } else {
-//           console.log('Failed')
-//           const objectUrl = {
-//             Name: findName,
-//             Url: findUrl,
-//             Status: status
-//           }
-//           console.log(objectUrl)
-//         }
-//       })
-
-//         .catch((error) => {
-//           console.log(error.message)
-//         })
-//     }
+// const findURL = (url,data)=>{
+//   return new Promise ((resolve,reject)=>{
+// const exp = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+// const string = /\[([^\]]*)\]/g;
+// const findUrl = data[i].match(exp)
+// const findStr = data[i].match(string)
+// const urlArray= []
+// findUrl.forEach(element => {
+// const request = findUrl[i] 
+// fetch(request).then((res)=> {
+//   let status = res.status;
+//   const extracttUrl = {
+//    Name: finds[i],
+//    Url: find[i],
+//    Status: status
 //   }
+// });
 // }
 
-module.exports={
-  Readme,
-  validate
-}
+  
+// })
+//   }
+
+
+//   const validate =()=>{
+//     let find = findUrl.match(exp);// hace la busqueda de url
+//     //console.log(find)
+//     let finds = findUrl.match(string);// hace busqueda de titulo
+//     //console.log(finds)
+    
+//     if(find){
+//      for(let i=0; i< find.length; i++){
+//        let request = find[i];
+//        fetch(request).then((res)=> {
+//          let status = res.status;
+//          const extracttUrl = {
+//           Name: finds[i],
+//           Url: find[i],
+//           Status: status
+//          }
+//          if(find){
+          
+//         //console.log('Url exitosa: ' + ' ' + extracttUrl.Name +' '+ extracttUrl.Url + '  status: ' + extracttUrl.Status);
+//          urlArraysuccess.push(extracttUrl)
+//          console.log(urlArraysuccess)
+//       }else{
+//           //console.log('fail' + extracttUrl);
+//           urlArrayfail.push(extracttUrl)
+//           console.log(urlArrayfail)
+//          }         
+//        }).catch((err)=>{
+//          console.log(err.message);
+//        })
+//      }
+//    }
+
+// }
+
+Readme ('../README.md')
+//.then(data)
+// .catch
+
+
+// module.exports={
+//   Readme,
+//   validate
+// }
+
+/////////////////////////////////
+
+// const fs = require('fs')
+// const fetch = require('node-fetch')
+// const path = require('path')
+// //const patron = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-/]))/gim
+// //const urlName = /\[([^\]]*)\]/gim
+// // const urlArray = []
+// // let dataIterada = ''
+// // let findUrl = ' '
+// // const urlFinder = ''
+// // const readMarkDown = ''
+// // let findName = ''
+
+// function readMarkDown (filePath) {
+//   return new Promise(function (resolve, reject) {
+//     fs.readFile(filePath, 'utf8', function (err, data) {
+//       if (err) {
+//         return reject(err)
+//       }
+//       // console.log(data)
+//       resolve(data)
+//     })
+//   })
+// }
+// function urlFinder (url, data) {
+//   return new Promise(function (resolve, reject) {
+//     fs.writeFile(url, data, function (err) {
+//       if (err) {
+//         return reject(err)
+//       }else{
+
+//       }
+      
+//       console.log(url)
+//       resolve(url)
+//     })
+//   })
+// }
+
+// readMarkDown('./Readme.md')
+//   .then(data => urlFinder('./cantidad.txt', data.length))
+//   .catch(err => console.log('Hubo un error' + err))
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const exp = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+// const string = /\[([^\]]*)\]/g
+// let findUrl = ''
+// let urlArraysuccess= []
+// let urlArrayfail=[]
+// const fs = require('fs');
+// const fetch = require('node-fetch');
+
+// const Readme = (callBack) => {
+//   fs.readFile('../Readme.md', 'utf8', (err, data) => {
+//     if (err) {
+//       console.log('Tienes un error')
+//     } else {
+//       callBack(data)     
+//     }
+//   })
+// }
+// Readme(callBack = (data) => {
+//   for (let i = 0; i < data.length; i++) {
+//       let total = data[i];
+//       findUrl += total;
+      
+//     }
+//   validate();
+//   });
+
+//   const validate =()=>{
+//     let find = findUrl.match(exp);// hace la busqueda de url
+//     //console.log(find)
+//     let finds = findUrl.match(string);// hace busqueda de titulo
+//     //console.log(finds)
+    
+//     if(find){
+//      for(let i=0; i< find.length; i++){
+//        let request = find[i];
+//        fetch(request).then((res)=> {
+//          let status = res.status;
+//          const extracttUrl = {
+//           Name: finds[i],
+//           Url: find[i],
+//           Status: status
+//          }
+//          if(find){
+          
+//         //console.log('Url exitosa: ' + ' ' + extracttUrl.Name +' '+ extracttUrl.Url + '  status: ' + extracttUrl.Status);
+//          urlArraysuccess.push(extracttUrl)
+//          console.log(urlArraysuccess)
+//       }else{
+//           //console.log('fail' + extracttUrl);
+//           urlArrayfail.push(extracttUrl)
+//           console.log(urlArrayfail)
+//          }         
+//        }).catch((err)=>{
+//          console.log(err.message);
+//        })
+//      }
+//    }
+
+// }
+
+
+// module.exports={
+//   Readme,
+//   validate
+// }
